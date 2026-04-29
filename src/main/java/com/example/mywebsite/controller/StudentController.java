7c5e8cb074cf85591da822f9814072c28925c461
alaga <alagarmca006@gmail.com>
2026-04-29 10:23:37 +0530

    updated 1

--- src/main/java/com/example/mywebsite/controller/StudentController.java
+++ src/main/java/com/example/mywebsite/controller/StudentController.java
@@ -38,5 +38,10 @@ public class StudentController {
         return studentRepository.save(newStudent);
     }
 
+    @GetMapping("/all")
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
 
 }
